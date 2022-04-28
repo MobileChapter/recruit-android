f820412ae2435b018d34e2086027305fe67cb0f1
Temir Atambayev <a.temirlan@gmail.com>
2022-04-29 01:00:19 +1200

    Implement transaction details screen

--- app/src/main/java/nz/co/test/transactions/ui/fragments/TransactionDetailsFragment.kt
+++ app/src/main/java/nz/co/test/transactions/ui/fragments/TransactionDetailsFragment.kt
@@ -1,32 +1,51 @@
 package nz.co.test.transactions.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import nz.co.test.transactions.data.services.Transaction
 import nz.co.test.transactions.databinding.FragmentTransactionDetailsBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class TransactionDetailsFragment : Fragment() {
 
     private lateinit var binding : FragmentTransactionDetailsBinding
+    private val ONE_HUNDRED = BigDecimal(100)
+    private val GST_AMOUNT = BigDecimal(15)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentTransactionDetailsBinding.inflate(inflater, container, false)
-        setupUI()
+        val bundle = arguments
+        val args = TransactionDetailsFragmentArgs.fromBundle(checkNotNull(bundle))
+        setupUI(args.transactionDetails)
         return binding.root
     }
 
-    private fun setupUI() {
-        //retrieve transaction details
-        //parse to UI
+    private fun setupUI(transactionDetails: Transaction) {
+        binding.transactionId.text = "ID:"+transactionDetails.id.toString()
+        binding.transactionSummaryText.text =transactionDetails.summary
+        binding.transactionCreditTxt.text = "Credit:$" + transactionDetails.credit.setScale(2).toPlainString()
+        binding.transactionDebitTxt.text = "Debit:$" + transactionDetails.debit.setScale(2).toPlainString()
+        binding.transactionGST.text = "GST:$" + calculateGST(transactionDetails)
+    }
+
+    private fun calculateGST(transactionDetails: Transaction): String {
+        return if(transactionDetails.debit.compareTo(BigDecimal.ZERO) == 0)
+            percentage(transactionDetails.credit)
+        else percentage(transactionDetails.debit)
+    }
+
+    private fun percentage(base: BigDecimal): String {
+        return base.multiply(GST_AMOUNT).divide(ONE_HUNDRED).setScale(2, RoundingMode.HALF_DOWN).toPlainString()
     }
 
     companion object {
-        @JvmStatic
-        fun newInstance() = TransactionDetailsFragment()
+        @JvmStatic fun newInstance() = TransactionDetailsFragment()
     }
 }
