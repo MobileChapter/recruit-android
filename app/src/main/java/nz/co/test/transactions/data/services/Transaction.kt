f1294c2ee9d30d51aee9ba2f647db1126d12b1ad
Temir Atambayev <a.temirlan@gmail.com>
2022-04-29 00:57:36 +1200

    Add ability to transfer arguments between fragments using nav component safeargs

--- app/src/main/java/nz/co/test/transactions/data/services/Transaction.kt
+++ app/src/main/java/nz/co/test/transactions/data/services/Transaction.kt
@@ -1,13 +1,15 @@
 package nz.co.test.transactions.data.services
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
-import java.time.OffsetDateTime
 
+@Parcelize
 data class Transaction(
     @field:Json(name = "id") val id: Int,
     @field:Json(name = "transactionDate") val transactionDate: String,
     @field:Json(name = "summary") val summary: String,
     @field:Json(name = "debit") val debit: BigDecimal,
     @field:Json(name = "credit") val credit: BigDecimal
-)
+) : Parcelable
